573bb6a7de786b51d9418be45b6e47555d61b6b7
Jani Salo <janikristian.salo@gmail.com>
2022-01-12 11:19:01 +0200

    Task1 done

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StatusBar, StyleSheet} from 'react-native';
+import {SafeAreaView} from "react-native";
+import List from "./components/list";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <List />
+      </SafeAreaView>
+      <StatusBar style="auto"/>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }});
+
+export default App;
+
---
+++ components/listItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import PropTypes from 'prop-types';
+
+
+const ListItem = ({singleMedia}) => {
+
+  return (
+    <TouchableOpacity style={[styles.row]}>
+      <Image
+        style={{width: 120, height: 250, margin: 10}}
+        source={{uri: singleMedia.thumbnails.w160}}
+      />
+      <View>
+        <Text style={{fontWeight: 'bold', padding: 5, margin: 10}}>{singleMedia.title}</Text>
+        <Text style={{width: 210}}>{singleMedia.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    margin: 2,
+    backgroundColor: 'lightgrey'
+  }
+});
+export default ListItem;
