3adcedfb9fa488ccda9139279de239f7a08a4abf
Jani Salo <janikristian.salo@gmail.com>
2022-01-27 09:33:54 +0200

    avatar created

--- views/Profile.js
+++ views/Profile.js
@@ -1,15 +1,56 @@
-import React, {useContext} from 'react';
-import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {StyleSheet, SafeAreaView, Text, Button, Image} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useTag} from '../hooks/ApiHooks';
+import {uploads} from '../utils/variables';
 
 const Profile = () => {
   const {setIsLoggedIn, user} = useContext(MainContext);
+  const [avatar, setAvatar] = useState('http://placekitten.com/640');
+  const {postTag, getFilesByTag} = useTag();
   console.log('Profile', user);
+
+  const fetchAvatar = async () => {
+    try {
+      const avatarArray = await getFilesByTag('avatar_' + user.user_id);
+      const avatar = avatarArray.pop();
+      setAvatar(uploads + avatar.filename);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  // quick'n'dirty testing postTag with hardcoded token
+  // this is not needed yet and should be called only when you want to set
+  // a new avatar in the remote API
+  const createAvatar = async (mediaId) => {
+    const data = {
+      file_id: mediaId,
+      tag: 'avatar_' + user.user_id,
+    };
+    try {
+      const result = await postTag(data, 'correct token should be here to use this');
+      console.log(result);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchAvatar();
+    // createAvatar(95); this was just for testing
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
       <Text>{user.username}</Text>
+      <Image
+        source={{uri: avatar}}
+        style={{width: '80%', height: '50%'}}
+        resizeMode="contain"
+      />
       <Text>{user.email}</Text>
       <Text>{user.full_name}</Text>
       <Button
